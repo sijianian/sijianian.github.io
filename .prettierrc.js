149c73d8519e266841a9d962a107e1ff66e20508
moke <moke@xiaoman.cn>
2019-08-28 10:46:35 +0800

    chore: init

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-// Prettier configuration
-// https://prettier.io/docs/en/configuration.html
-module.exports = {
-  singleQuote: true,
-  semi: false,
-  trailingComma: "es5"
-};
--- source/categories/index.md
+++
@@ -1,5 +0,0 @@
----
-title: 分类
-date: 2017-09-11 10:53:14
-type: "categories"
----
